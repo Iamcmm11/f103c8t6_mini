ba55929a1d521fa038eace59a013a36aefa5a75c
Iamcmm11 <icescuc@gmail.com>
2026-06-10 18:52:08 +0800

    【需求】增加CAN的底层配置，还未进行.d

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "can.h"
 #include "dma.h"
 #include "i2c.h"
 #include "spi.h"
@@ -105,6 +106,7 @@ int main(void)
   MX_TIM5_Init();
   MX_TIM6_Init();
   MX_TIM2_Init();
+  MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
